8dff34fb18f4b2f5c5d9d84a7cfd2c60db989008
Alain Tardif <tardif.alain@gmail.com>
2024-02-22 20:14:16 +0100

    modif et delete ligne 5

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(HttpClientInterface $httpClient): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
